ed6ba4d3f5e53a5caf179d940792b5e3e862d52a
Pieter <superpieter88@gmail.com>
2021-06-17 15:45:58 +0200

    Location +Better health
    Added the players' location and better health.

--- src/main/java/nl/devpieter/betternametags/mixin/EntityRendererMixin.java
+++ src/main/java/nl/devpieter/betternametags/mixin/EntityRendererMixin.java
@@ -39,19 +39,22 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Matrix4f;
 
-
 @Mixin(EntityRenderer.class)
 public abstract class EntityRendererMixin<T extends Entity> {
 
 	MinecraftClient minecraft = MinecraftClient.getInstance();
-		
+
 	@Inject(method = "renderLabelIfPresent", at = @At("TAIL"))
-	protected void renderLabelIfPresent(T entity, Text text, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo callbackInfo) {
+	protected void renderLabelIfPresent(T entity, Text text, MatrixStack matrices,
+			VertexConsumerProvider vertexConsumers, int light, CallbackInfo callbackInfo) {
 		if (entity instanceof PlayerEntity) {
 			PlayerEntity player = (PlayerEntity) entity;
 
 			DecimalFormat df = new DecimalFormat("#.#");
-			Text infoRowOne = new LiteralText(String.format("§c❤ %s §a↔ %s", df.format(player.getHealth()), df.format(player.distanceTo(minecraft.player))));
+			Text infoRow1 = new LiteralText(String.format("%s \u00A7a↔ %s", getHealth(player, df),
+					df.format(player.distanceTo(minecraft.player))));
+			Text infoRow2 = new LiteralText(String.format("\u00A7d%s %s %s", df.format(player.getPos().x),
+					df.format(player.getPos().y), df.format(player.getPos().z)));
 
 			double d = minecraft.getEntityRenderDispatcher().getSquaredDistanceToCamera(player);
 			if (d <= 4096.0D) {
@@ -61,45 +64,50 @@ public abstract class EntityRendererMixin<T extends Entity> {
 
 				float backgroundOpacity = minecraft.options.getTextBackgroundOpacity(0.25F);
 				int backgroundColor = (int) (backgroundOpacity * 255.0F) << 24;
-				
-				float f = player.getHeight() + 0.5F;				
+
+				float f = player.getHeight() + 0.5F;
 				matrices.push();
 				matrices.translate(0.0D, (double) f, 0.0D);
 				matrices.multiply(minecraft.getEntityRenderDispatcher().getRotation());
 				matrices.scale(-0.025F, -0.025F, 0.025F);
 				Matrix4f matrix4f = matrices.peek().getModel();
-				
-				float infoRowOneX = (float) (-textRenderer.getWidth((StringVisitable) infoRowOne) / 2);
+
+				float infoRow1X = (float) (-textRenderer.getWidth((StringVisitable) infoRow1) / 2);
+				float infoRow2X = (float) (-textRenderer.getWidth((StringVisitable) infoRow2) / 2);
 				float textX = (float) (-textRenderer.getWidth((StringVisitable) text) / 2);
 				int y = -10;
-								
-				textRenderer.draw(infoRowOne, infoRowOneX, (float) y, 553648127, false, matrix4f, vertexConsumers, isNotSneaky, backgroundColor, light);
-				
+
+				// Render name tag
+				textRenderer.draw(infoRow1, infoRow1X, (float) y - 10, 553648127, false, matrix4f, vertexConsumers, isNotSneaky, backgroundColor, light);
+				textRenderer.draw(infoRow2, infoRow2X, (float) y, 553648127, false, matrix4f, vertexConsumers, isNotSneaky, backgroundColor, light);
+
 				if (isNotSneaky) {
-					textRenderer.draw(infoRowOne, infoRowOneX, (float) y, -1, false, matrix4f, vertexConsumers, false, 0, light);
-										
+					// Render name tag
+					textRenderer.draw(infoRow1, infoRow1X, (float) y - 10, -1, false, matrix4f, vertexConsumers, false, 0, light);
+					textRenderer.draw(infoRow2, infoRow2X, (float) y, -1, false, matrix4f, vertexConsumers, false, 0, light);
+
+					// Render players head
 					Identifier texture = nameToTexture(minecraft, player.getName().asString());
-					if(texture != null) {
-						
+					if (texture != null) {
 						RenderSystem.enableDepthTest();
 						minecraft.getTextureManager().bindTexture(texture);
 						DrawableHelper.drawTexture(matrices, (int) textX - 11, y + 9, 10, 10, 8.0F, 8.0F, 8, 8, 64, 64);
 						RenderSystem.enableBlend();
 						DrawableHelper.drawTexture(matrices, (int) textX - 11, y + 9, 10, 10, 40.0F, 8.0F, 8, 8, 64, 64);
 						RenderSystem.disableBlend();
-						RenderSystem.disableDepthTest();			
+						RenderSystem.disableDepthTest();
 					}
 				}
 				matrices.pop();
 			}
 		}
 	}
-	
+
 	private static Identifier nameToTexture(MinecraftClient minecraft, String name) {
 		if (minecraft.player.world == null)
 			return null;
 
-		Identifier foundSkin = null;		
+		Identifier foundSkin = null;
 		for (PlayerListEntry playerList : minecraft.player.networkHandler.getPlayerList()) {
 			if (playerList.getProfile().getName().equalsIgnoreCase(name)) {
 				foundSkin = playerList.getSkinTexture();
@@ -107,5 +115,21 @@ public abstract class EntityRendererMixin<T extends Entity> {
 		}
 		return foundSkin;
 	}
-	
+
+	private static String getHealth(PlayerEntity player, DecimalFormat df) {
+		float Health = player.getHealth() + player.getAbsorptionAmount();
+		if (Health > 20)
+			return " \u00A76❤" + df.format(Health);
+		else if (Health >= 15.0 && Health <= 20.9)
+			return " \u00A7a❤" + df.format(Health);
+		else if (Health >= 10.0 && Health <= 14.9)
+			return " \u00A7e❤" + df.format(Health);
+		else if (Health >= 5.0 && Health <= 9.9)
+			return " \u00A7c❤" + df.format(Health);
+		else if (Health >= 1.0 && Health <= 4.9)
+			return " \u00A74❤" + df.format(Health);
+		else
+			return " \u00A70❤" + df.format(Health);
+	}
+
 }
